cfb7f01064bef47141b410d3d1f06e04de1f8272
Edouard <edou68@gmail.com>
2024-11-18 00:54:37 +0100

    fix: use lodash for randomness

--- pablobot.js
+++ pablobot.js
@@ -1,21 +1,30 @@
-const { Client, MessageAttachment } = require("discord.js");
-const client = new Client();
+const { Client, GatewayIntentBits, MessageAttachment } = require("discord.js");
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
 const config = require("./config.json");
 const fs = require("fs");
+const _ = require("lodash");
 
-const files = fs
-  .readdirSync("./resources/")
-  .filter((e) => e.endsWith(".jpg") || e.endsWith(".png"));
+// const files = fs
+//   .readdirSync("./resources/")
+//   .filter((e) => ['.png','.jpg','.mov','.mp4'].some(extension => e.endsWith(extension)));
 const cooldown = new Set();
 
-let pictures = files;
+var pictures = [];
+var picturesQueue = [];
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", (msg) => {
-  if (msg.content != config.command) return;
+client.on("messageCreate", (msg) => {
+  if (!msg.content.startsWith(config.command) || msg.author.bot) return;
 
   if (cooldown.has(msg.author.id)) {
     msg.channel.send(config.cooldownMessage);
@@ -27,13 +36,14 @@ client.on("message", (msg) => {
     cooldown.delete(msg.author.id);
   }, config.cooldownTime);
 
-  let chosenPic = pictures[Math.floor(Math.random() * pictures.length)];
+  let picture = picturesQueue.shift();
+  msg.reply({ files: [picture] });
 
-  pictures = pictures.filter((e) => e !== chosenPic);
-  if (pictures.length <= 0) pictures = files;
-
-  let pic = new MessageAttachment("./resources/" + chosenPic);
-  msg.channel.send(pic);
+  if (picturesQueue.length == 0) picturesQueue = _.shuffle(pictures);
 });
 
+files = _.map(fs.readdirSync('./resources'), (file) => './resources/' + file);
+pictures = pictures.concat(files);
+
+picturesQueue = _.shuffle(pictures);
 client.login(config.token);
